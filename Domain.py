db1ae1f48c0d8577aac7b2c81244f568a300da01
magnusgun15@ru.is <magnusgun15@ru.is>
2016-12-14 16:21:21 +0000

    skeleton created

--- Domain.py
+++ Domain.py
@@ -1,4 +1,5 @@
 import subprocess
+import platform
 
 def compileCPlus():
     compilationProcess = subprocess.Popen([r"/usr/bin/g++","test.cpp","-o","output.exe"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -8,4 +9,18 @@ def compileCPlus():
 
     return output
 
+def runCPlus(pairs,inputFile):
+    pass
+
+def removeFile(inputFile):
+    pass
+
+def testFile(inputFile,testStrings):
+    compileCPlus()
+    runCPlus()
+    removeFile()
+    pass
+
 #compileCPlus()
+
+print(platform.system())
